9c6f767b84e7bbba05ec0789a49b6726ac406956
amrhalawanii <amrhalawanii@gmail.com>
2025-12-16 18:29:56 +0300

    Update Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,19 +18,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/logo-c.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/logo-c.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/logo-c.png",
+        type: "image/png+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/logo-c.png",
   },
 }
 
